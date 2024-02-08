60e71a6dccd0f3aa30a6ef785f5bf0087ee7f87e
XIAO-PC\xiaoc <xiaochai.joe@gmail.com>
2024-02-07 23:47:43 -0800

    fix loading bug

---
+++ src/field/Potential/Potential.m
@@ -0,0 +1,24 @@
+classdef (Abstract) Potential < handle
+    %POTENTIAL Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (SetAccess = protected)
+        Atom Atom
+        Name string
+    end
+    
+    methods
+        function obj = Potential(atom,name)
+            %POTENTIAL Construct an instance of this class
+            %   Detailed explanation goes here
+            arguments
+                atom Atom
+                name string
+            end
+            obj.Atom = atom;
+            obj.Name = name;
+        end
+        
+    end
+end
+
