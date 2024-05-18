932bb9c99a486f47a69f03f4ee6ee04a40db7264
WOODHOUSE <xiaochai.joe@gmail.com>
2024-05-17 17:56:40 -0700

    fixed amplitude bugs in setting keysight ARB files

--- src/math/Waveform/Waveform.m
+++ src/math/Waveform/Waveform.m
@@ -6,7 +6,7 @@ classdef (Abstract) Waveform < handle
         SamplingRate double {mustBePositive} = 1 % In Hz
         StartTime double = 0 % Start time, in s
         Duration double {mustBePositive} % How long of the waveform, in s
-        Amplitude double {mustBeNonnegative} = 0 % Peak-to-peak amplitude, usually in Volts.
+        Amplitude double = 0 % Peak-to-peak amplitude, usually in Volts.
         Offset double = 0 % Offest, usually in Volts.
         Scan table = table(string.empty,string.empty, 'VariableNames',{'ParameterName','VariableName'}) % For scanning in Hardware control
     end
